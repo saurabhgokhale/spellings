453591a86ec55e34633b4b4611e84ab6201dc85b
Saurabh Gokhale <saurabh_gokhale@apple.com>
2021-09-05 16:43:24 -0700

    working example

--- src/app/practice/practice.component.ts
+++ src/app/practice/practice.component.ts
@@ -23,39 +23,73 @@ export class PracticeComponent implements OnInit {
   public wrongCount = 0;
   private response: any;
   private retrievedObject: any;
-  private index = 0;
+  private correctSet : any[] = [];
+  private wrongSet: any[] = [];
   public spelling = '';
+
 
   constructor(private http: HttpClient,
               private router: Router, 
               private activatedRoute: ActivatedRoute) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.http.get('https://spellings-dcs.s3.ap-south-1.amazonaws.com/spelling.txt',{ responseType: 'text' as 'json'})
     .subscribe(
       data => {
         console.log(data);
         this.response = data;
         this.words = this.response.split(',');
-        this.words.push('watermelon');
-
         // Retrieve the object from storage
-        if(localStorage.getItem('spellObject') == null) {
-          this.retrievedObject = { 'correct': [], 'wrong': [], 'wordCount': 0, 'correctCount': 0, 'wrongCount': 0, 'index': 0};
+
+        if(localStorage.getItem('spellv1') == null) {
+          let correctSet: any[] = [];
+          let wrongSet: any[] = [];
+          this.retrievedObject = { 'correct': correctSet, 'wrong': wrongSet, 'wordCount': 0, 'correctCount': 0, 'wrongCount': 0, 'index': 0};
           // Put the object into storage
-          localStorage.setItem('spellObject', JSON.stringify(this.retrievedObject));
+          //this.setWithExpiry('spellv1', this.retrievedObject, 5000);
+          localStorage.setItem('spellv1', JSON.stringify(this.retrievedObject));
         }
-        this.retrievedObject = JSON.parse(localStorage.getItem('spellObject')|| '');
+        this.retrievedObject = JSON.parse(localStorage.getItem('spellv1')|| '');
         console.log('retrievedObject: ', this.retrievedObject);
         this.totalWords = this.words.length;
         this.correctCount = this.retrievedObject['correct'].length;
         this.wrongCount = this.retrievedObject['wrong'].length;
-        this.index = this.retrievedObject['wordCount'];
-        console.log("index: " + this.index);
-        this.word = this.words[this.index]; //'watermelon';
+        this.correctSet = this.retrievedObject['correct'];
+        this.wrongSet = this.retrievedObject['wrong'];
+        this.word = this.getNextWord(this.words, this.retrievedObject['wrong']);
         this.charWidth = this.word.length * (1 + 0.5);
         this.spelling = '';
     })
+  }
+
+  setWithExpiry(key: string, value: any, ttl: number) {
+    const now = new Date()
+  
+    // `item` is an object which contains the original value
+    // as well as the time when it's supposed to expire
+    const item = {
+      value: value,
+      expiry: now.getTime() + ttl,
+    }
+    localStorage.setItem(key, JSON.stringify(item))
+  }
+
+  getWithExpiry(key: string) {
+    const itemStr = localStorage.getItem(key)
+    // if the item doesn't exist, return null
+    if (!itemStr) {
+      return null;
+    }
+    const item = JSON.parse(itemStr)
+    const now = new Date()
+    // compare the expiry time of the item with the current time
+    if (now.getTime() > item.expiry) {
+      // If the item is expired, delete the item from storage
+      // and return null
+      localStorage.removeItem(key)
+      return null;
+    }
+    return item.value
   }
 
   onKey(spelling: string) {
@@ -63,31 +97,57 @@ export class PracticeComponent implements OnInit {
     else this.valueEntered = true;
   }
 
-  checkSpelling(spelling: string): void {
+  checkSpelling(spelling: string) {
     console.log(spelling == this.word);
+    let correctArr = this.retrievedObject['correct'];
+    let wrongArr = this.retrievedObject['wrong'];
     if(spelling === this.word) {
       this.fontColor = "green";
-      this.retrievedObject['correct'].push(this.word);
-      this.retrievedObject['correctCount'] = this.retrievedObject['correct'].length;
-      this.correctCount = this.retrievedObject['correct'].length;
+      let i = wrongArr.indexOf(this.word);
+      if( i >= 0) {
+        wrongArr.splice(i, 1, this.word);
+      }
+      correctArr.push(this.word);
     }
     else {
       this.fontColor = "red";
-      this.retrievedObject['wrong'].push(this.word);
-      this.retrievedObject['wrongCount'] = this.retrievedObject['wrong'].length;
-      this.wrongCount = this.retrievedObject['wrong'].length;
+      let i = correctArr.indexOf(this.word);
+      if( i >= 0) {
+        correctArr.splice(i, 1, this.word);
+      }
+      wrongArr.push(this.word);
     }
-    localStorage.setItem('spellObject', JSON.stringify(this.retrievedObject));
+    this.retrievedObject['wrong'] = wrongArr;
+    this.retrievedObject['correct'] = correctArr;
+    this.correctCount = correctArr.length;
+    this.wrongCount = wrongArr.length;
+
+    localStorage.setItem('spellv1', JSON.stringify(this.retrievedObject));
   }
 
-  nextWord(): void {
-    this.index++;
-    this.retrievedObject['index'] = this.index;
-    localStorage.setItem('spellObject', JSON.stringify(this.retrievedObject));
+  nextWord(): any {
+    //this.retrievedObject['index'] = this.index;
+    localStorage.setItem('spellv1', JSON.stringify(this.retrievedObject));
     this.spelling = '';
     this.fontColor = "dodgerblue";
-    this.word = this.words[this.index];
+    this.word = this.getNextWord(this.words, this.retrievedObject['wrong']);
     this.charWidth = this.word.length * (1 + 0.5);
-  }  
+  }
 
+  getNextWord(words: any[], wrongWords: Set<String> ) {
+    this.retrievedObject = JSON.parse(localStorage.getItem('spellv1')|| '');
+    let correctArr = this.retrievedObject['correct'];
+    let wrongArr = this.retrievedObject['wrong'];
+    let newWord = '';
+    while(true) {
+      let index = Math.floor(Math.random() * words.length);
+      newWord = words[index];
+      let i = correctArr.indexOf(newWord);
+      let j = wrongArr.indexOf(newWord);
+      if(i < 0 && j < 0) {
+        break;
+      }
+    }
+    return newWord;
+  }
 }
